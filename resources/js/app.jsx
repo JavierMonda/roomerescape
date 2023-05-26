5bd75359efb6efb44d2fae258111cc115d5652d7
JavierJG <javier.raidpc@gmail.com>
2023-05-26 20:44:44 +0100

    Update app.jsx
    Solución error

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,19 +5,21 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
-// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-        return pages[`./Pages/${name}.jsx`]
-    },
-    setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
-    },
+	resolve: (name) =>resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+
+  setup({ el, App, props }) {
+
+    const root = createRoot(el);
+
+    root.render(<App{...props}/>);
+
+  },
+
 });
 
 document.getElementById("app").removeAttribute("data-page");
